dd462ab4c63a026650d70486b6d8df54d6280d35
Sam Grundman <samuel@yodas.ws>
2016-10-13 01:17:16 -0400

    Tab shift

--- www/pokédex/move_effects.js
+++ www/pokédex/move_effects.js
@@ -26,68 +26,68 @@ pokemon.data.move_effects = {
 			pokemon.battle.log("No effect&hellip;")
 		}
 	},
-moveData(effect_id) {
-	var effect = {}
-	// Increase Critical Hit Stage
-	switch (effect_id) {
-	case 44:
-	case 201:
-	case 210:
-		effect.criticalHitStage = 1
-		break;
-	case 289:
-		effect.criticalHitStage = 4
-		break;
-	}
-	// Alter Battle Stats Boosts
-	effect.boost = {}
-	switch (effect_id) {
-	case 11:
-		effect.boost.num = 1
-		effect.boost.stat = 'atk'
-		break;
-	case 12:
-		effect.boost.num = 1
-		effect.boost.stat = 'def'
-		break;
-	case 14:
-		effect.boost.num = 1
-		effect.boost.stat = 'spatk'
-		break;
-	case 17:
-		effect.boost.num = 1
-		effect.boost.stat = 'evasion'
-		break;
-	case 19:
-		effect.boost.num = -1
-		effect.boost.stat = 'atk'
-		break;
-	case 20:
-		effect.boost.num = -1
-		effect.boost.stat = 'def'
-		break;
-	case 21:
-		effect.boost.num = -1
-		effect.boost.stat = 'spd'
-		break;
-	case 24:
-		effect.boost.num = -1
-		effect.boost.stat = 'accuracy'
-		break;
-	case 25:
-		effect.boost.num = -1
-		effect.boost.stat = 'evasion'
-		break;
-	}
-	// Alter Status Conditions
-	switch (effect_id) {
-	case 2:
-		// Put Target to Sleep!
-		effect.changeStatus = (target) => {
-			pokemon.data.move_effects.setStatus('sleep', target)
+	moveData(effect_id){
+		var effect = {}
+		// Increase Critical Hit Stage
+		switch (effect_id) {
+		case 44:
+		case 201:
+		case 210:
+			effect.criticalHitStage = 1
+			break;
+		case 289:
+			effect.criticalHitStage = 4
+			break;
 		}
-		break;
+		// Alter Battle Stats Boosts
+		effect.boost = {}
+		switch (effect_id) {
+		case 11:
+			effect.boost.num = 1
+			effect.boost.stat = 'atk'
+			break;
+		case 12:
+			effect.boost.num = 1
+			effect.boost.stat = 'def'
+			break;
+		case 14:
+			effect.boost.num = 1
+			effect.boost.stat = 'spatk'
+			break;
+		case 17:
+			effect.boost.num = 1
+			effect.boost.stat = 'evasion'
+			break;
+		case 19:
+			effect.boost.num = -1
+			effect.boost.stat = 'atk'
+			break;
+		case 20:
+			effect.boost.num = -1
+			effect.boost.stat = 'def'
+			break;
+		case 21:
+			effect.boost.num = -1
+			effect.boost.stat = 'spd'
+			break;
+		case 24:
+			effect.boost.num = -1
+			effect.boost.stat = 'accuracy'
+			break;
+		case 25:
+			effect.boost.num = -1
+			effect.boost.stat = 'evasion'
+			break;
+		}
+		// Alter Status Conditions
+		switch (effect_id) {
+		case 2:
+			// Put Target to Sleep!
+			effect.changeStatus = (target) => {
+				pokemon.data.move_effects.setStatus('sleep', target)
+			}
+			break;
+		}
+		return effect
 	}
-	return effect
-}
 }
